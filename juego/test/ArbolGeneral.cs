eedc25813dd969aad9921202cc30d94f258f41ee
Claudio Bernal <bernalclaudio044@gmail.com>
2023-11-13 23:06:10 -0300

    first commit(Consulta 1)

---
+++ juego/test/ArbolGeneral.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+
+namespace DeepSpace
+{
+	public class ArbolGeneral<T>
+	{
+		
+		private T dato;
+		private List<ArbolGeneral<T>> hijos = new List<ArbolGeneral<T>>();
+
+		public ArbolGeneral(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public List<ArbolGeneral<T>> getHijos() {
+			return hijos;
+		}
+	
+		public void agregarHijo(ArbolGeneral<T> hijo) {
+			this.getHijos().Add(hijo);
+		}
+	
+		public void eliminarHijo(ArbolGeneral<T> hijo) {
+			this.getHijos().Remove(hijo);
+		}
+	
+		public bool esHoja() {
+			return this.getHijos().Count == 0;
+		}
+	
+		public int altura() {
+			if(this.esHoja())
+				return 0;
+			else{
+				int hmax=0;
+				foreach(var hijo in this.getHijos()){
+					int h= hijo.altura();
+					if(h>hmax){
+						hmax=h;
+					}	
+				}
+			return hmax +1;
+			}
+		}
+		
+		public bool includes(T dato){
+			if(this.getDatoRaiz().Equals(dato)) return true;
+			else{
+				foreach(var a in this.getHijos()){
+					if(a.includes(dato)) return a.includes(dato);
+				}
+				return false;
+			}
+		}
+		public int nivel(T dato) {
+			if(this.getDatoRaiz().Equals(dato)) return 0;
+			else{
+				foreach(var a in this.getHijos()){
+					if(a.includes(dato)) return 1 + a.nivel(dato);
+				}
+			}
+				
+			return -1;
+			
+		}
+	
+	}
+}
---
+++ juego/test/Estrategia.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace DeepSpace
+{
+
+	class Estrategia
+	{
+		
+		//Calcula y retorna un texto con la distancia del camino que existe
+		//entre el planeta del Bot y la raíz.
+		
+		public String Consulta1(ArbolGeneral<Planeta> arbol)
+		{
+			var c = new Cola<ArbolGeneral<Planeta>>();
+			ArbolGeneral<Planeta> arbolAux;
+			int distancia = 0;
+			
+			// encolamos raiz
+			c.encolar(arbol);
+			// encolamos null
+			c.encolar(null);						
+			
+			// procesamos cola
+			while(!c.esVacia()){
+				arbolAux = c.desencolar();
+				
+				if(arbolAux == null){
+					if(!c.esVacia()){
+						c.encolar(null);											
+					}
+				}
+				else{
+					// procesar el dato
+					if(arbolAux.getDatoRaiz().EsPlanetaDeLaIA())
+						distancia= arbol.nivel(arbolAux.getDatoRaiz());
+				
+					// encolamos hijos
+					foreach(var hijo in arbolAux.getHijos())
+						c.encolar(hijo);
+				}				
+			}
+			return "La distancia del camino que existe entre el planeta del Bot y la raíz es de " + distancia;
+			
+		}
+		
+		//Retorna un texto con el listado de los planetas ubicados en todos los descendientes 
+		//del nodo que contiene al planeta del Bot.
+		
+		public String Consulta2( ArbolGeneral<Planeta> arbol)
+		{
+			
+			return "Implementar";
+		}
+
+
+		public String Consulta3( ArbolGeneral<Planeta> arbol)
+		{
+			return "Implementar";
+		}
+		
+		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
+		{
+			//Implementar
+			
+			return null;
+		}
+	}
+}
